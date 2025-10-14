fe160f0171f68ac108ba9bee2f533edd731fe36d
aminsafari <allanlondon19452122@gmail.com>
2025-10-14 22:17:49 +0330

    10-14-2025 elastic searchs

--- searchs/apps.py
+++ searchs/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class SearchsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "searchs"
+    def ready(self):
+        import searchs.signals
