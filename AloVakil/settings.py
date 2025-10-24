5ab1f08e7a4ab047955371969375155f4baa175e
Amin safari <130644891+thedragonbody@users.noreply.github.com>
2025-10-24 16:11:30 +0330

    Integrate IDPay payment request flow

--- AloVakil/settings.py
+++ AloVakil/settings.py
@@ -27,6 +27,7 @@ DEBUG = env.bool('DEBUG', default=False)
 IDPAY_API_KEY = env.str("IDPAY_API_KEY", default=None)
 IDPAY_SANDBOX = env.bool("IDPAY_SANDBOX", default=True)
 IDPAY_API_URL = env.str("IDPAY_API_URL", default="https://api.idpay.ir/v1.1")
+IDPAY_CALLBACK_URL = env.str("IDPAY_CALLBACK_URL", default=None)
 PAYMENT_AMOUNT_MULTIPLIER = env.int("PAYMENT_AMOUNT_MULTIPLIER", default=1)
 
 # SMS
